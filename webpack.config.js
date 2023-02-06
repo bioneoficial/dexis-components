f5f26fef3d0eac9d5261fcc031b9b333a520cb4f
Thais Rocha <thais.rocha@ciandt.com>
2023-02-06 17:52:43 -0300

    test: change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -38,8 +38,22 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        use: ["source-map-loader"],
-        enforce: "pre"
+        enforce: 'pre',
+        use: 
+        [
+          {
+            //needed to chain sourcemaps.  see: https://webpack.js.org/loaders/source-map-loader/
+            loader: 'source-map-loader',
+            options: {
+              filterSourceMappingUrl: (url, resourcePath) => {
+                if (/.*\/node_modules\/.*/.test(resourcePath)) {
+                    return false
+                }
+                return true
+              }
+            }
+          }
+        ],
       },
       {
         test: /\.(scss|css)$/,
